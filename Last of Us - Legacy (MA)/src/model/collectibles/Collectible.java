ec139846accb3dc872f4f7547fb51b28d27466b6
YousefElbrolosy <90988100+YousefElbrolosy@users.noreply.github.com>
2023-04-30 18:38:33 +0200

    Delete Last of Us - Legacy (MA) directory

--- Last of Us - Legacy (MA)/src/model/collectibles/Collectible.java
+++
@@ -1,9 +0,0 @@
-package model.collectibles;
-import exceptions.NoAvailableResourcesException;
-import model.characters.*;
-
-public interface Collectible  {
-    void pickUp(Hero h) throws NoAvailableResourcesException ;
-    void use(Hero h) throws NoAvailableResourcesException;
-
-}
