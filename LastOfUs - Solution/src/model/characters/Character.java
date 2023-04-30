64fb395e4b56db93e0747b27807cc43ef6d98d36
Ahmedsameh19 <ahmed.sameh7433@gmail.com>
2023-04-30 23:39:04 +0300

    is dead

--- LastOfUs - Solution/src/model/characters/Character.java
+++ LastOfUs - Solution/src/model/characters/Character.java
@@ -92,5 +92,11 @@ public abstract class Character {
 
 	}
 	
-
+	public void onCharacterDeath(Character dead){
+		//Handling when health reaches zero is done in other methods where Health is reached 0
+		if (dead.getCurrentHp()<=0){
+			dead.getLocation() = null;
+		}
+	}
+	
 }
