d2f979242570b807074a72fedd65091b588f4aa5
mohamedhamouda55 <mohamedhamouda55@gmail.com>
2023-05-09 11:33:19 +0200

    kosom useSpecial

--- LastOfUs - Solution/src/model/characters/Explorer.java
+++ LastOfUs - Solution/src/model/characters/Explorer.java
@@ -1,5 +1,12 @@
 package model.characters;
 
+import java.util.ArrayList;
+
+import engine.Game;
+import exceptions.InvalidTargetException;
+import exceptions.NoAvailableResourcesException;
+import exceptions.NotEnoughActionsException;
+import model.collectibles.Supply;
 
 public class Explorer extends Hero {
 	
@@ -8,7 +15,26 @@ public class Explorer extends Hero {
 		super( name, maxHp,  attackDmg,  maxActions) ;
 		
 	}
-
+    public void useSpecial() throws NoAvailableResourcesException,NotEnoughActionsException,InvalidTargetException{
+		ArrayList<Supply>x=this.getSupplyInventory();
+		if(x.isEmpty())
+		   throw new NoAvailableResourcesException();
+        if (this.getActionsAvailable()==0||this.isSpecialAction()==false)
+		  throw new NotEnoughActionsException();
+		  if (this.getTarget() instanceof Hero)
+		    throw new InvalidTargetException();
+		if (this.getTarget() instanceof Hero)
+		    throw new InvalidTargetException();
+		
+			for(int i=0;i<15;i++){
+				for(int j=0;j<15;j++){
+				Game.map[i][j].setVisible(true);	
+			}
+		}
+			
+			
+			super.attack();
+	}
 	
 	
 
--- LastOfUs - Solution/src/model/characters/Medic.java
+++ LastOfUs - Solution/src/model/characters/Medic.java
@@ -1,6 +1,11 @@
 package model.characters;
 
+import java.util.ArrayList;
 
+import exceptions.InvalidTargetException;
+import exceptions.NoAvailableResourcesException;
+import exceptions.NotEnoughActionsException;
+import model.collectibles.Supply;
 
 public class Medic extends Hero {
 	//Heal amount  attribute - quiz idea
@@ -8,10 +13,24 @@ public class Medic extends Hero {
 
 	public Medic(String name,int maxHp, int attackDmg, int maxActions) {
 		super( name, maxHp,  attackDmg,  maxActions) ;
-		
-		
 	}
-	
+	public void useSpecial() throws NoAvailableResourcesException, NotEnoughActionsException,InvalidTargetException {
+		if (this.getTarget() instanceof Zombie)
+		   throw new InvalidTargetException();
+		ArrayList<Supply>x=this.getSupplyInventory();
+		if(x.isEmpty())
+		   throw new NoAvailableResourcesException();
+        if (this.getActionsAvailable()==0||this.isSpecialAction()==false)
+		  throw new NotEnoughActionsException();
+		if (this.getCurrentHp() < this.getMaxHp()) {
+			this.setCurrentHp(this.getMaxHp());
+		} else {
+			this.getTarget().setCurrentHp(this.getTarget().getMaxHp());
+		}
+		
+		
+		super.attack();
+	}
 
 
 }
